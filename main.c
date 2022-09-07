34c6dacac42801b8062311587b6e26d54e15961a
yudhapandusetiawan <112883560+yudhaps1@users.noreply.github.com>
2022-09-07 12:24:51 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Nama : Yudha Pandu Setiawan");
+    printf("NIM : A11.2022.14118");
+    printf("Alamat : Ds.Banyumanis RT05 RW06 Kec.Donorojo Kab.Jepara");
+    printf("Asal Sekolah : Smk Tunas Harapan Pati");
+    return 0;
+}
