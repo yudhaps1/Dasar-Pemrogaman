2b8f4d8dd6727871bb3fcc954079d6a106438f8a
yudhapandusetiawan <112883560+yudhaps1@users.noreply.github.com>
2022-09-12 17:32:49 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+
+    int x = 19;
+    float t = 155;
+    char g = 'L';
+    string ub = "XXL";
+
+    cout << "==========================================" << endl;
+    cout << "Nama Saya         : Yudha Pandu Setiawan" << endl;
+    cout << "Umur Saya         : " << x;
+    cout << "\nTinggi Saya       : " << t;
+    cout << "\nukuran baju saya  : " << g;
+    cout << "\nnukuran baju ayah : " << ub;
+    cout << "\n==========================================" << endl;
+    return 0;
+}
