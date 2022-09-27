bfb62ce2d3411ff0517fea0ce952b330386382c4
yudhapandusetiawan <112883560+yudhaps1@users.noreply.github.com>
2022-09-27 21:08:01 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,38 +1,48 @@
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
 int main()
 {
+    int a, b;
+    string jurusan [3] = {"Teknik Informatika","Sistem Informasi","Desain Komunikasi Visual"};
+    string Mahasiswa [4] = {"Freshman","Shopomore","Junior","Senior"};
 
-    float pi = 3.14;
-    float a;
-    float b;
-    float c;
+    cout << "===Daftar Jurusan===" << endl;
+    cout << "1. " << jurusan[0] << endl;
+    cout << "2. " << jurusan[1] << endl;
+    cout << "3. " << jurusan[2] << endl << endl;
 
-    cout << "==============================";
-    cout << "\nmasukkan jari-jari lingkaran : " << endl;
-     cin >> a;
-    cout << "\nmasukkan jari-jari lingkaran : " << endl;
-    cin >> b;
-    c=pi*a*b;
-    cout << "hasil luas lingkaran adalah : " <<c;
+    cout << "===Kategori Mahasiswa===" << endl;
+    cout << "1. Semester 1-2 = " << Mahasiswa[0] << endl;
+    cout << "2. Semester 3-4 = " << Mahasiswa[1] << endl;
+    cout << "3. Semester 5-6 = " << Mahasiswa[2] << endl;
+    cout << "4. Semester 7-8 = " << Mahasiswa[3] << endl << endl;
 
-    cout << "\n==============================";
+    cout << " Masukkan Jurusan Anda : "; cin >> a;
+    cout << " Masukkan Semester Anda : "; cin >> b;
+    if (b>=1 && b<=2) {
+        cout << "Jurusan Anda adalah" << jurusan[a-1] << "." << endl;
+        cout << "Anda Dalam Kategori" << Mahasiswa[0] << "." << " year." << endl;
 
-    float q;
-    float r;
-    float t;
+    }
+    else if (b>=3 && b<=4) {
+        cout << "Jurusan Anda adalah" << jurusan[a-1] << "." << endl;
+        cout << "Anda Dalam Kategori" << Mahasiswa[1] << "." << " year." << endl;
+    }
+     else if (b>=5 && b<=6) {
+        cout << "Jurusan Anda adalah" << jurusan[a-1] << "." << endl;
+        cout << "Anda Dalam Kategori" << Mahasiswa[2] << "." << " year." << endl;
+     }
+     else if (b>=7 && b<=8) {
+        cout << "Jurusan Anda adalah" << jurusan[a-1] << "." << endl;
+        cout << "Anda Dalam Kategori" << Mahasiswa[3] << "." << " year." << endl;
+     }
+        else{
+            cout << "Pilihan Anda Salah!!!" << endl;
+        }
 
-    cout << "\nmasukkan panjang persegi panjang : " << endl;
-     cin >> q;
-    cout << "\nmasukkan lebar persegi panjang : " << endl;
-     cin >> t;
-
-    r=q*t;
-    cout << "luas persegi panjang adalah : " <<r;
-    cout << "\n==============================";
-    cout << "\nSELESAI..";
     return 0;
 }
