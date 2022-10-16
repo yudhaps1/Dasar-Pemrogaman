c0f169402dc6be52d7270a5fd88786b407fcf293
yudhapandusetiawan <112883560+yudhaps1@users.noreply.github.com>
2022-10-16 21:19:28 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,48 +1,27 @@
 #include <iostream>
-#include <stdio.h>
-#include <string.h>
 
 using namespace std;
 
 int main()
 {
-    int a, b;
-    string jurusan [3] = {"Teknik Informatika","Sistem Informasi","Desain Komunikasi Visual"};
-    string Mahasiswa [4] = {"Freshman","Shopomore","Junior","Senior"};
-
-    cout << "===Daftar Jurusan===" << endl;
-    cout << "1. " << jurusan[0] << endl;
-    cout << "2. " << jurusan[1] << endl;
-    cout << "3. " << jurusan[2] << endl << endl;
-
-    cout << "===Kategori Mahasiswa===" << endl;
-    cout << "1. Semester 1-2 = " << Mahasiswa[0] << endl;
-    cout << "2. Semester 3-4 = " << Mahasiswa[1] << endl;
-    cout << "3. Semester 5-6 = " << Mahasiswa[2] << endl;
-    cout << "4. Semester 7-8 = " << Mahasiswa[3] << endl << endl;
-
-    cout << " Masukkan Jurusan Anda : "; cin >> a;
-    cout << " Masukkan Semester Anda : "; cin >> b;
-    if (b>=1 && b<=2) {
-        cout << "Jurusan Anda adalah" << jurusan[a-1] << "." << endl;
-        cout << "Anda Dalam Kategori" << Mahasiswa[0] << "." << " year." << endl;
-
-    }
-    else if (b>=3 && b<=4) {
-        cout << "Jurusan Anda adalah" << jurusan[a-1] << "." << endl;
-        cout << "Anda Dalam Kategori" << Mahasiswa[1] << "." << " year." << endl;
-    }
-     else if (b>=5 && b<=6) {
-        cout << "Jurusan Anda adalah" << jurusan[a-1] << "." << endl;
-        cout << "Anda Dalam Kategori" << Mahasiswa[2] << "." << " year." << endl;
-     }
-     else if (b>=7 && b<=8) {
-        cout << "Jurusan Anda adalah" << jurusan[a-1] << "." << endl;
-        cout << "Anda Dalam Kategori" << Mahasiswa[3] << "." << " year." << endl;
-     }
-        else{
-            cout << "Pilihan Anda Salah!!!" << endl;
+    int n, f1 = 0, f2 = 1, berikutnya = 0;
+    cout << "masukkan batas deret bilangan fibonacci : " << endl;
+    cin >> n;
+    cout << "deret fibonacci : " << endl;
+    for (int i = 1;i <=n; i++) {
+        if (i == 1) {
+            cout << "" << f1 << "";
+            continue;
         }
+        if (i == 2) {
+            cout << f2 << "";
+            continue;
+        }
+        berikutnya = f1 + f2;
+        f1 = f2;
+        f2 = berikutnya;
+        cout << berikutnya << "";
+    }
 
     return 0;
 }
